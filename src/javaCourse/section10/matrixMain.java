7ae5aa75ae040d392184f6719bf5b84baca2e9b6
Jair Cunha <Jair@pbsenac.intra>
2025-06-26 11:58:30 -0300

    feat: matrix exercise (gotta study later)

---
+++ src/javaCourse/section10/matrixMain.java
@@ -0,0 +1,43 @@
+package javaCourse.section10;
+
+import java.util.Scanner;
+
+public class matrixMain {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Type the length of the matrix: ");
+        int n = sc.nextInt();
+        int[][] matrix = new int[n][n];
+
+        // Reading the matrix
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < n; j++){
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        // Printing the diagonal
+        System.out.println("Main diagonal:");
+        for (int i = 0; i < n; i++){
+            System.out.print(matrix[i][i] + " ");
+        }
+
+        // Printing the amount of negative numbers
+
+        int negativeNum = 0;
+
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < n; j++){
+                if (matrix[i][j] < 0){
+                    negativeNum++;
+                }
+            }
+        }
+
+        System.out.println();
+        System.out.println("Negative numbers: " + negativeNum);
+
+        sc.close();
+    }
+}
